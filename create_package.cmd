2813c3f8a422a76425888eeadfba8c82db52e8af
v0lt <mpc-be@mail.ru>
2024-10-06 20:54:50 +0300

    Добавлен create_package.cmd.

---
+++ create_package.cmd
@@ -0,0 +1,87 @@
+@ECHO OFF
+SETLOCAL
+CD /D %~dp0
+
+REM -------------------------------------
+
+IF NOT EXIST "out\ReleaseAMD64\VirtualDub64.exe" (
+  ECHO VirtualDub2 not compiled.
+  GOTO :END
+)
+
+IF NOT EXIST "..\avlib_vdplugin\_bin\Release_x64\avlib-1.vdplugin" (
+  ECHO avlib_vdplugin not compiled.
+  GOTO :END
+)
+
+IF NOT EXIST "..\avlib_vdplugin\_bin\ffmpeg\avcodec-61.dll" (
+  ECHO FFmpeg DLLs not downloaded.
+  GOTO :END
+)
+
+IF NOT EXIST "..\scripted_vdplugin\_bin\Release_x64\scripted\scripted.vdplugin" (
+  ECHO scripted_vdplugin not compiled.
+  GOTO :END
+)
+
+REM -------------------------------------
+
+FOR /F "tokens=2*" %%A IN (
+  'REG QUERY "HKLM\SOFTWARE\7-Zip" /v "Path" 2^>NUL ^| FIND "REG_SZ" ^|^|
+   REG QUERY "HKLM\SOFTWARE\Wow6432Node\7-Zip" /v "Path" 2^>NUL ^| FIND "REG_SZ"') DO SET "SEVENZIP=%%B\7z.exe"
+
+IF NOT EXIST "%SEVENZIP%" (
+  ECHO 7Zip not found.
+  GOTO :END
+)
+
+REM -------------------------------------
+
+SET PCKG_DIR=out\VirtualDub2_v2.1
+
+MKDIR %PCKG_DIR%
+MKDIR %PCKG_DIR%\extra
+MKDIR %PCKG_DIR%\plugins64
+
+COPY /Y /V "docs\Changes.txt"                   "%PCKG_DIR%\Changes.txt"
+COPY /Y /V "LICENSE.txt"                        "%PCKG_DIR%\LICENSE.txt"
+COPY /Y /V "Readme.md"                          "%PCKG_DIR%\Readme.md"
+COPY /Y /V "out\ReleaseAMD64\vdub64.exe"        "%PCKG_DIR%\vdub64.exe"
+COPY /Y /V "out\ReleaseAMD64\VirtualDub64.exe"  "%PCKG_DIR%\VirtualDub64.exe"
+COPY /Y /V "out\ReleaseAMD64\VirtualDub64.vdi"  "%PCKG_DIR%\VirtualDub64.vdi"
+COPY /Y /V "dist\VirtualDub.chm"                "%PCKG_DIR%\VirtualDub.chm"
+
+COPY /Y /V "dist\extra\."                       "%PCKG_DIR%\extra\."
+COPY /Y /V "out\ReleaseAMD64\auxsetup.exe"      "%PCKG_DIR%\extra\auxsetup.exe"
+COPY /Y /V "out\ReleaseAMD64\vdicmdrv.dll"      "%PCKG_DIR%\extra\vdicmdrv.dll"
+COPY /Y /V "out\ReleaseAMD64\vdlaunch64.exe"    "%PCKG_DIR%\extra\vdlaunch64.exe"
+COPY /Y /V "out\ReleaseAMD64\vdremote64.dll"    "%PCKG_DIR%\extra\vdremote64.dll"
+COPY /Y /V "out\ReleaseAMD64\vdsvrlnk64.dll"    "%PCKG_DIR%\extra\vdsvrlnk64.dll"
+COPY /Y /V "dist\plugins64\."                   "%PCKG_DIR%\plugins64\."
+															    
+COPY /Y /V "..\avlib_vdplugin\_bin\ffmpeg\avcodec-61.dll"      "%PCKG_DIR%\avcodec-61.dll"
+COPY /Y /V "..\avlib_vdplugin\_bin\ffmpeg\avformat-61.dll"     "%PCKG_DIR%\avformat-61.dll"
+COPY /Y /V "..\avlib_vdplugin\_bin\ffmpeg\avutil-59.dll"       "%PCKG_DIR%\avutil-59.dll"
+COPY /Y /V "..\avlib_vdplugin\_bin\ffmpeg\swresample-5.dll"    "%PCKG_DIR%\swresample-5.dll"
+COPY /Y /V "..\avlib_vdplugin\_bin\ffmpeg\swscale-8.dll"       "%PCKG_DIR%\swscale-8.dll"
+
+COPY /Y /V "..\avlib_vdplugin\_bin\Release_x64\avlib-1.vdplugin"  "%PCKG_DIR%\plugins64\avlib-1.vdplugin"
+COPY /Y /V "..\avlib_vdplugin\Readme.md"                          "%PCKG_DIR%\plugins64\avlib-1_Readme.md"
+COPY /Y /V "..\avlib_vdplugin\history.txt"                        "%PCKG_DIR%\plugins64\avlib-1_history.txt"
+
+COPY /Y /V "..\scripted_vdplugin\_bin\Release_x64\scripted\scripted.vdplugin" "%PCKG_DIR%\plugins64\scripted.vdplugin"
+COPY /Y /V "..\scripted_vdplugin\Readme.md"                                   "%PCKG_DIR%\plugins64\scripted_Readme.md"
+COPY /Y /V "..\scripted_vdplugin\history.txt"                                 "%PCKG_DIR%\plugins64\scripted_history.txt"
+
+REM -------------------------------------
+
+FOR /F "USEBACKQ" %%F IN (`powershell -NoLogo -NoProfile -Command ^(Get-Item "%PCKG_DIR%\VirtualDub64.exe"^).VersionInfo.FileVersion`) DO (SET FILE_VERSION=%%F)
+
+SET PCKG_NAME=VirtualDub2_v%FILE_VERSION%
+
+"%SEVENZIP%" a -m0=lzma -mx9 -ms=on "out\%PCKG_NAME%.7z" ".\%PCKG_DIR%"
+
+:END
+ENDLOCAL
+TIMEOUT /T 5
+EXIT /B
