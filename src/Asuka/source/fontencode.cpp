ada20726839b85355cfc3cbefc1d53834b88a795
v0lt <mpc-be@mail.ru>
2024-12-29 15:49:38 +0300

    #define WINVER 0x0600, #define _WIN32_WINNT 0x0600 Убрано описание IFileOpenDialog.

--- src/Asuka/source/fontencode.cpp
+++ src/Asuka/source/fontencode.cpp
@@ -17,8 +17,8 @@
 
 #include "stdafx.h"
 
-#define WINVER 0x0500
-#define _WIN32_WINNT 0x0500
+#define WINVER 0x0600
+#define _WIN32_WINNT 0x0600
 #include <vd2/system/memory.h>
 #include <vd2/system/vectors.h>
 #include <vd2/system/vdstl.h>
