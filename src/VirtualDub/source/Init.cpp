866b7ade7581925d2f0b65d900540b74c8fac2dc
v0lt <mpc-be@mail.ru>
2025-09-24 22:14:51 +0300

    Дополнение к commit 16ccbc5. Уменьшено количество сравнений (спасибо lordnn).

--- src/VirtualDub/source/Init.cpp
+++ src/VirtualDub/source/Init.cpp
@@ -1098,17 +1098,21 @@ int ProcessCommandLine::scan(const VDCommandLine& cmdLine, const bool execute) {
 					if (execute) SetUnhandledExceptionFilter(NULL);
 				}
 				else if (!wcscmp(token, L"hexedit")) {
-					if (cmdLine.GetNextNonSwitchArgument(it, token)) {
-						if (execute) HexEdit(NULL, token, false);
-					} else {
-						if (execute) HexEdit(NULL, NULL, false);
+					if (execute) {
+						if (cmdLine.GetNextNonSwitchArgument(it, token)) {
+							HexEdit(NULL, token, false);
+						} else {
+							HexEdit(NULL, NULL, false);
+						}
 					}
 				}
 				else if (!wcscmp(token, L"hexview")) {
-					if (cmdLine.GetNextNonSwitchArgument(it, token)) {
-						if (execute) HexEdit(NULL, token, true);
-					} else {
-						if (execute) HexEdit(NULL, NULL, true);
+					if (execute) {
+						if (cmdLine.GetNextNonSwitchArgument(it, token)) {
+							HexEdit(NULL, token, true);
+						} else {
+							HexEdit(NULL, NULL, true);
+						}
 					}
 				}
 				else if (!wcscmp(token, L"i")) {
