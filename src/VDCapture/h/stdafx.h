1c9e3f08e6f8af07a4c11752f0ed1eb3d02d3d51
v0lt <mpc-be@mail.ru>
2026-08-12 21:10:13 +0300

    Удален #pragma warning(disable: 4819).

--- src/VDCapture/h/stdafx.h
+++ src/VDCapture/h/stdafx.h
@@ -1,8 +1,2 @@
-// Compilation workaround for high char annoyance in the Platform SDK:
-//
-// c:\platsdk5\include\uuids.h : warning C4819: The file contains a character that cannot
-// be represented in the current code page (932). Save the file in Unicode format to
-// prevent data loss
-#pragma warning(disable: 4819)
 
 #include <vd2/system/vdtypes.h>
